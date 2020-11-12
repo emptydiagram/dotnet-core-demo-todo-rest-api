440bb303dc2e3d1fb9f71da765f0651c405dbc63
llamachannel <llamachannel@fastmail.com>
2020-11-11 22:00:06 -0500

    Add wouter demo

--- ClientApp/src/components/App.tsx
+++ ClientApp/src/components/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link, Route, Switch } from 'wouter';
+
 import TodoListApp from './TodoListApp';
 
 function App() {
   return (
-    <TodoListApp />
+    <div>
+      <Link href="/login">login</Link>
+      <Link href="/todo">todo</Link>
+
+      <Switch>
+        <Route path="/login">
+          <p>TODO: login stuff</p>
+        </Route>
+        <Route path="/todo">
+          <TodoListApp />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
