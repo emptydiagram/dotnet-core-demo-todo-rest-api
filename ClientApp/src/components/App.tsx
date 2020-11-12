ca65593a12d337bea36d5b8367f89714945bddc2
llamachannel <llamachannel@fastmail.com>
2020-11-11 19:49:07 -0500

    Remove starter app code

---
+++ ClientApp/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TodoListApp from './TodoListApp';
+
+function App() {
+  return (
+    <TodoListApp />
+  );
+}
+
+export default App;
---
+++ ClientApp/src/components/TodoListApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TodoListApp.css';
+
+function TodoListApp() {
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <p>TODO: A todo list app</p>
+    </div>
+  );
+}
+
+export default TodoListApp;
